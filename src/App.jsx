af4af5cd2d358ef35eb3cbc582939f5c091e7d51
marcossadu <masadu@outlook.es>
2023-06-02 08:31:58 +0200

    barras

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import ProgressBar from "./components/ProgressBar";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>SinguReact Components</h1>
+      <ProgressBar color={"red"} progress={"90%"} title={"HTML"} />
+      <ProgressBar color={"blue"} progress={"80%"} title={"CSS"}/>
+      <ProgressBar color={"green"} progress={"65%"} title={"JAVASCRIPT"}/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,5 +1,15 @@
-import './ProgressBar.css';
+import "./ProgressBar.css";
 
-export default function ProgressBar({ title, color, progress }) {
-    return <h1>Implementame!!</h1>
+export default function ProgressBar({ title, color, progress}) {
+  return (
+    <div className="divNerio">
+      <div
+        className="divSamaneh"
+        style={{ backgroundColor: color, height: "4rem", width: progress }}
+      >
+
+        {title} <span>{progress}</span>
+      </div>
+    </div>
+  );
 }
